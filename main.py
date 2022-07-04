eb627f0f99a9015c00ef1dd4838dec75c8534d26
Valurauta <wncjf2000@naver.com>
2022-07-04 13:31:55 +0900

    3ㄷ

--- main.py
+++ main.py
@@ -37,16 +37,46 @@ browser = generate_chrome(
     download_path=DOWNLOAD_DIR)
 
 url = 'https://pp.kepco.co.kr/intro.do'
-browser.get(url)
+browser.get(url) #url 주소로 접속
 time.sleep(5)
 
-# elm = browser.execute_script("document.getElementsByName('RSA_USER_ID')[0].value = \'" + siteID + "\'") 
-# elm = browser.execute_script("document.getElementsByName('RSA_USER_PWD')[0].value = \'" + sitePW + "\'")
-elm = browser.find_element("xpath", '//*[@id="RSA_USER_ID"]')
+elm = browser.find_element("xpath", '//*[@id="RSA_USER_ID"]') #아이디 입력
 elm.send_keys(siteID) 
-elm = browser.find_element("xpath", '//*[@id="RSA_USER_PWD"]')
+elm = browser.find_element("xpath", '//*[@id="RSA_USER_PWD"]') #비밀번호 입력
 elm.send_keys(sitePW)
-elm = browser.find_element("xpath", '//*[@id="intro_form"]/form/fieldset/input[1]').click()
+elm = browser.find_element("xpath", '//*[@id="intro_form"]/form/fieldset/input[1]').click() #로그인 버튼 클릭
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="smart2"]/a').click() #실시간사용량-시간대별 상단 버튼 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="txt"]/div[2]/div/p[1]/img').click() #날짜 선택 펼치기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="ui-datepicker-div"]/div/div/select[1]/option[1]').click() #년도 찾아 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="ui-datepicker-div"]/div/div/select[2]/option[6]').click() #월 찾아 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//a[@class="ui-state-default" and text()="11"]').click() #날짜 찾아 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="txt"]/div[2]/div/p[2]/span[1]/a').click() #조회 버튼 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="txt"]/div[2]/div/p[1]/img').click() #날짜 선택 펼치기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="ui-datepicker-div"]/div/a[2]/span').click() #다음 달 버튼 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//a[@class="ui-state-default" and text()="5"]').click() #날짜 찾아 누르기
+time.sleep(5)
+
+elm = browser.find_element("xpath", '//*[@id="txt"]/div[2]/div/p[2]/span[1]/a').click() #조회 버튼 누르기
+time.sleep(5)
+
 
 time.sleep(10)
 # time.sleep(7 + random.randrange(1, 8))
