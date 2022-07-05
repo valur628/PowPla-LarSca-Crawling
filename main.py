7e55287f4cc31baa1d37c65131602920bc13f283
Valurauta <wncjf2000@naver.com>
2022-07-05 13:33:11 +0900

    sadas

--- main.py
+++ main.py
@@ -38,14 +38,14 @@ def splitTimes():
     return (split_year_month)
 
 
-RANGEMONTH = 12  # 크롤링할 개월 수(끝나는 기간 아님)
+RANGEMONTH = 3  # 크롤링할 개월 수(끝나는 기간 아님)
 
 PROJECT_DIR = str(os.path.dirname(os.path.abspath(__file__)))
 DOWNLOAD_DIR = f'{PROJECT_DIR}/download'
 DATABASE_DIR = f'{PROJECT_DIR}/database'
 driver_path = f'{PROJECT_DIR}/lib/webDriver/'
 
-electricity_time_columns_15 = [0]
+electricity_time_columns_15 = []
 current_time = datetime.datetime(2020, 1, 1)
 for i in range(15, 24*60, 15):
     current_time = current_time + datetime.timedelta(minutes=15)
@@ -53,7 +53,7 @@ for i in range(15, 24*60, 15):
 electricity_time_columns_15.append('24:00')
 print(electricity_time_columns_15)
 
-electricity_time_columns_60 = [0]
+electricity_time_columns_60 = []
 current_time = datetime.datetime(2020, 1, 1)
 for i in range(0, 24*60-60, 60):
     current_time = current_time + datetime.timedelta(minutes=60)
@@ -107,17 +107,20 @@ elm = browser.find_element(
     "xpath", '//a[@class="ui-state-default" and text()="16"]').click()  # 날짜 찾아 누르기
 time.sleep(1)
 
+first_days = []
+end_days = []
 split_year_month = splitTimes()
 elm = browser.find_element(
     "xpath", '//*[@id="txt"]/div[2]/div/p[1]/img').click()  # 날짜 선택 펼치기
 
 break_check = False
+first_check = False
 electricity_list = []
-electricity_df = pd.DataFrame(electricity_list, columns=electricity_time_columns_15)
+electricity_df = pd.DataFrame(index=electricity_time_columns_15)
 
-for i in range(1, RANGEMONTH):  # 탐색이 이루어지는 개월 범위
+for i in range(1, RANGEMONTH+1):  # 탐색이 이루어지는 개월 범위
     number_day = getMonthRange(split_year_month[0], split_year_month[1])
-    for j in range(1, number_day+1):  # 탐색이 이루어지는 요일 범위
+    for j in range(1, 28+1):  # 탐색이 이루어지는 요일 범위
         time.sleep(1)
 
         # pass_text = browser.find_element("xpath", '//td[@id="F_AP_QT"]').text
@@ -133,22 +136,31 @@ for i in range(1, RANGEMONTH):  # 탐색이 이루어지는 개월 범위
         elm = browser.find_element(
             "xpath", '//*[@id="txt"]/div[2]/div/p[2]/span[1]/a').click()  # 조회 버튼 누르기
         time.sleep(4)
+        
+        if first_check == False:
+            first_days = splitTimes()
+            first_check = True
+        
         # 표 제작 시작
 
         table_15 = browser.find_element(By.ID, 'tableListChart')  # 15분짜리 테이블 경로
         tbody_15 = table_15.find_element(By.TAG_NAME, "tbody")
         rows_15 = tbody_15.find_elements(By.TAG_NAME, "tr")
-        print(splitTimes())
         for index, value in enumerate(rows_15):
             head = value.find_elements(By.TAG_NAME, "th")[0]
             body = value.find_elements(By.TAG_NAME, "td")[0]
             electricity_list.append(body.text)
-            print(body.text)
         for index, value in enumerate(rows_15):
             head = value.find_elements(By.TAG_NAME, "th")[1]
             body = value.find_elements(By.TAG_NAME, "td")[7]
             electricity_list.append(body.text)
-            print(body.text)
+        elm_day = browser.find_element(
+            "xpath", '//*[@id="SELECT_DT"]')  # 현재 년도와 월 그리고 일 함께 가져오기
+        now_year_month_day = elm_day.get_attribute('value')  # elm에서 value만 빼내기
+        print(now_year_month_day)
+        electricity_df[now_year_month_day] = electricity_list
+        print(electricity_df)
+        electricity_list.clear()
         # 표 제작 종료
         elm = browser.find_element(
             "xpath", '//*[@id="txt"]/div[2]/div/p[1]/img').click()  # 날짜 선택 펼치기
@@ -167,6 +179,11 @@ for i in range(1, RANGEMONTH):  # 탐색이 이루어지는 개월 범위
     elm = browser.find_element(
         "xpath", '//*[@id="txt"]/div[2]/div/p[1]/img').click()  # 날짜 선택 펼치기
     time.sleep(1)
+end_days = splitTimes()
+first_days_str = "".join(first_days)
+end_days_str = "".join(end_days)
 
+electricity_df_transpose = electricity_df.transpose()
+electricity_df_transpose.to_csv("database/electData__{0}_{1}.csv", mode='w').format(first_days_str, end_days_str)
 time.sleep(10)
 # time.sleep(7 + random.randrange(1, 8))
